b44d449d04cafe601e9806b03590a755001843cb
BUNN1E5 <bunn1e5@users.noreply.github.com>
2024-08-04 06:56:03 -0700

    Things are starting to work (I hate myself!!)

--- game_systems/interactable.gd
+++ game_systems/interactable.gd
@@ -6,8 +6,12 @@ class_name Interactable
 var mousePath = "/root/Main/Mouse"
 @onready var mouse : PlayerMouse = get_node(mousePath)
 
+
+
 signal button_pressed()
 
+var default_scale = self.scale
+var hover_scale = default_scale * 1.2
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.monitoring = true
@@ -26,8 +30,10 @@ func _input(event):
 func _process(delta):
 	if self.overlaps_area(mouse.selector_area):
 		#change sprite in some way
+		self.scale = lerp(self.scale, hover_scale, 8 * delta)
 		pass
 	else:
 		#change it back
+		self.scale = lerp(self.scale, default_scale, 8 * delta)
 		pass
 	pass
